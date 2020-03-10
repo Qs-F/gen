e343d715d4ed65b7311de72317ec1a315a1a9b8c
Qs-F <qsf@de-liker.com>
2020-03-11 04:19:53 +0900

    Add test for GenList

--- lib/gen/gen_test.go
+++ lib/gen/gen_test.go
@@ -14,7 +14,48 @@ var (
 	base = filepath.Join("..", "..", "testdata")
 )
 
-func Test_Unit(t *testing.T) {
+func TestGenList(t *testing.T) {
+	tests := []struct {
+		Path    string
+		Loaders []gen.Loader
+		Root    string
+
+		Output gen.List
+	}{
+		{
+			Path:    filepath.Join(base, "imports", "md"),
+			Loaders: []gen.Loader{markdown.NewMarkdownLoader()},
+			Root:    "index.md",
+
+			Output: gen.List{
+				"index.md": gen.Variables{
+					"title":   "Index Page",
+					"import":  []interface{}{"list.md"},
+					"Content": "\n# {{ .title }}\n\n## this article is for test\n\n{{ .committers.gopher }} wrote.\n",
+				},
+				"list.md": gen.Variables{
+					"committers": map[string]interface{}{"gopher": "Gopher Google"},
+					"Content":    "",
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		list, err := gen.GenList(test.Path, test.Loaders...)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if !reflect.DeepEqual(list, test.Output) {
+			t.Errorf("want: \n%s\n, but got: \n%s\n", spew.Sdump(test.Output), spew.Sdump(list))
+		} else {
+			t.Logf("got: \n%s\n", spew.Sdump(list))
+		}
+	}
+}
+
+func TestUnit_GenList_ResolveKey(t *testing.T) {
 	tests := []struct {
 		Path    string
 		Loaders []gen.Loader
@@ -36,7 +77,7 @@ func Test_Unit(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		list, err := gen.New(test.Path, "").List(test.Loaders...)
+		list, err := gen.GenList(test.Path, test.Loaders...)
 		if err != nil {
 			t.Error(err)
 			continue
