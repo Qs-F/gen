d2546f51200e8ab1b5e379ef596b2e3084ef9c66
Qs-F <qsf@de-liker.com>
2020-03-11 05:00:54 +0900

    Add Expand

--- lib/gen/gen.go
+++ lib/gen/gen.go
@@ -1,8 +1,23 @@
 // Package gen is the packeg of internal gen
 package gen
 
+import (
+	"bytes"
+	"errors"
+	"html/template"
+)
+
 // Variables is the type expressing front matters
 type Variables map[string]interface{}
+
+const (
+	ContentIdent = "Content"
+)
+
+var (
+	// ErrContentNotFound is returned when Field 'Content' is not found in Variables
+	ErrContentNotFound = errors.New("Field 'Content' is not found in Variables")
+)
 
 // Gen is the struct for gen cmd
 type Gen struct {
@@ -16,4 +31,28 @@ func New(basePath string, layoutPath string) *Gen {
 		BasePath:   basePath,
 		LayoutPath: layoutPath,
 	}
+}
+
+func Expand(v Variables) (string, error) {
+	ti, ok := v[ContentIdent]
+	if !ok {
+		return "", ErrContentNotFound
+	}
+	t, ok := ti.(string)
+	if !ok {
+		return "", ErrContentNotFound
+	}
+
+	tmpl, err := template.New("page").Parse(t)
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, v)
+	if err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
