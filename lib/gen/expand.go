8fdb14674ece7d40d67f1f4bbe3b21c6a0e00579
Qs-F <qsf@de-liker.com>
2020-03-21 04:16:13 +0900

    Add Expand

--- lib/gen/expand.go
+++ lib/gen/expand.go
@@ -12,6 +12,52 @@ type Expander interface {
 	Expand(p []byte, v Variables) ([]byte, error)
 }
 
+func (list List) Expand(basePath, srcPath, dstPath string, expanders ...Expander) error {
+	dst := filepath.Join(basePath, dstPath)
+	exps := make(map[string]Expander)
+	for _, expander := range expanders {
+		from, _ := expander.Ext()
+		exps[from] = expander
+	}
+
+	for file := range list {
+		// check file is in src
+		if !strings.HasPrefix(file, srcPath) {
+			continue
+		}
+		// check expander is available for file
+		cont := false
+		var expander Expander
+		for ext, exp := range exps {
+			if ext == filepath.Ext(file) {
+				cont = true
+				expander = exp
+				break
+			}
+		}
+		if !cont {
+			continue
+		}
+
+		content, perm, err := open(filepath.Join(basePath, srcPath, file))
+		if err != nil {
+			return err
+		}
+
+		to, b, err := ExpandEach(list, expander, file, content)
+		if err != nil {
+			return err
+		}
+
+		err = ioutil.WriteFile(filepath.Join(dst, to), b, perm)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func ExpandEach(list List, exp Expander, file string, content []byte) (dst string, w []byte, err error) {
 	v, err := ResolveKey(list, file)
 	if err != nil {
@@ -26,4 +72,22 @@ func ExpandEach(list List, exp Expander, file string, content []byte) (dst strin
 	_, ext := exp.Ext()
 	dst = strings.TrimRight(file, filepath.Ext(file)) + ext
 	return dst, w, nil
+}
+
+func open(path string) ([]byte, os.FileMode, error) {
+	f, err := os.Open(filepath.Join(path))
+	if err != nil {
+		return nil, os.ModePerm, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, os.ModePerm, err
+	}
+	perm := fi.Mode().Perm()
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, os.ModePerm, err
+	}
+	f.Close()
+	return content, perm, nil
 }
