c6c0951fc92f7639f070648ef4d7014b54f90343
Qs-F <qsf@de-liker.com>
2020-03-20 19:34:18 +0900

    Add Expand for html

--- lib/expander/html/expand.go
+++ lib/expander/html/expand.go
@@ -1,19 +1,26 @@
 // Package html is implementation of gen/lib/gen.Expander for html
 package html
 
-import "github.com/Qs-F/gen/lib/gen"
+import (
+	"bytes"
+	"html/template"
+
+	"github.com/Qs-F/gen/lib/gen"
+)
 
 const (
-	from = "html"
+	from = "phtml"
 	to   = "html"
 )
 
 // HTML is struct for html package to implement Expander
-type HTML struct{}
+type HTML struct {
+	ContentKey string
+}
 
 // New returns new instance of HTML
-func New() *HTML {
-	return &HTML{}
+func New(key string) *HTML {
+	return &HTML{ContentKey: key}
 }
 
 // Ext implements gen/lib/gen.Expander
@@ -23,5 +30,18 @@ func (_ *HTML) Ext() (string, string) {
 
 // Expand implements gen/lib/gen.Expander
 func (_ *HTML) Expand(p []byte, v gen.Variables) ([]byte, error) {
-	return nil, nil
+	tmpl, err := template.New("page").Parse(string(p))
+	if err != nil {
+		return nil, err
+	}
+
+	tmpl = tmpl.Option("missingkey=error")
+
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, v)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
