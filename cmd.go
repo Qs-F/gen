6c65e048ad64d9d49d9bc517be8c7688a2b4cee9
Qs-F <qsf@de-liker.com>
2020-03-21 04:56:02 +0900

    Update cmd.go

--- cmd.go
+++ cmd.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"flag"
 	"os"
 
+	mde "github.com/Qs-F/gen/lib/expander/markdown"
 	"github.com/Qs-F/gen/lib/gen"
-	"github.com/Qs-F/gen/lib/loader/markdown"
-	"github.com/davecgh/go-spew/spew"
+	"github.com/Qs-F/gen/lib/loader/html"
+	mdl "github.com/Qs-F/gen/lib/loader/markdown"
 	"github.com/sirupsen/logrus"
 )
 
@@ -15,17 +17,32 @@ func main() {
 		logrus.Error(err)
 		return
 	}
+	base := flag.String("base", wd, "base path")
+	src := flag.String("src", wd, "src path")
+	dst := flag.String("dst", wd, "dst path")
+	flag.Parse()
 
-	list, err := gen.GenList(wd, markdown.New())
+	g := gen.New("", "", "")
+	err = g.Set(*base, *src, *dst)
 	if err != nil {
 		logrus.Error(err)
 		return
 	}
 
-	v, err := gen.ResolveKey(list, "testdata/basic/md/basic.md")
+	mdLd := mdl.New()
+	htmlLd := html.New("content")
+
+	list, err := gen.GenList(g.BasePath, mdLd, htmlLd)
 	if err != nil {
 		logrus.Error(err)
 		return
 	}
-	spew.Dump(v)
+
+	mdExp := mde.New("layout", "content", "__content__", list)
+
+	err = list.Expand(g.BasePath, g.SrcPath, g.DstPath, mdExp)
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
 }
