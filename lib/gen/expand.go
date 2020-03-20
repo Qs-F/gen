acc22bf88bf31941402f18006c7554eefcdefa51
Qs-F <qsf@de-liker.com>
2020-03-21 04:16:02 +0900

    Add ExpandEach

--- lib/gen/expand.go
+++ lib/gen/expand.go
@@ -1,6 +1,29 @@
 package gen
 
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Expander interface {
 	Ext() (from, to string)
 	Expand(p []byte, v Variables) ([]byte, error)
+}
+
+func ExpandEach(list List, exp Expander, file string, content []byte) (dst string, w []byte, err error) {
+	v, err := ResolveKey(list, file)
+	if err != nil {
+		return "", nil, err
+	}
+
+	w, err = exp.Expand(content, v)
+	if err != nil {
+		return "", nil, err
+	}
+
+	_, ext := exp.Ext()
+	dst = strings.TrimRight(file, filepath.Ext(file)) + ext
+	return dst, w, nil
 }
